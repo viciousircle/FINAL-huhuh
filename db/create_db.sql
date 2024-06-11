98e1cc1a1f5eea3f1e73df882fe02af81c6bce45
Vu Minh Quy <quy.vtm227257@sis.hust.edu.vn>
2024-06-11 13:20:34 +0700

    change some constraint

--- db/create_db.sql
+++ db/create_db.sql
@@ -21,17 +21,17 @@ DROP DATABASE LMS;
 
 -- Create the account table for users
 CREATE TABLE users.account (
-    admin_id VARCHAR(20) PRIMARY KEY,
-    [password] VARCHAR(20),
-    admin_name VARCHAR(100)
+    admin_id    VARCHAR(20) PRIMARY KEY,
+    [password]  VARCHAR(20),
+    admin_name  VARCHAR(100)
 );
 GO
 
 -- Create the history_edit table for user edits
 CREATE TABLE users.history (
-    edit_id INT PRIMARY KEY,
-    admin_id INT,
-    book_id INT,
+    edit_id     INT PRIMARY KEY,
+    admin_id    VARCHAR(20),
+    book_id     INT,
     [timestamp] DATETIME,
     FOREIGN KEY (admin_id) REFERENCES users.account(admin_id),
     FOREIGN KEY (book_id) REFERENCES books.bookMarc(book_id)
@@ -40,30 +40,30 @@ GO
 
 -- Create the guest table
 CREATE TABLE users.guest (
-    guest_id INT PRIMARY KEY,
-    guest_name VARCHAR(255),
+    guest_id    INT PRIMARY KEY,
+    guest_name  VARCHAR(100),
     [timestamp] DATETIME
 );
 GO
 
 -- Create the bookMarc table for book details
 CREATE TABLE books.bookMarc (
-    book_id INT IDENTITY(1,1),
-    title VARCHAR(255),
-    author VARCHAR(255),
+    book_id     INT IDENTITY(1,1),
+    title       VARCHAR(255),
+    author      VARCHAR(255),
     public_year INT,
     public_comp VARCHAR(255),
-    isbn VARCHAR(255) UNIQUE,
+    isbn        VARCHAR(25) UNIQUE,
     PRIMARY KEY (book_id, isbn)
 );
 GO
 
 -- Create the book table for book inventory
 CREATE TABLE books.book (
-    warehouse_id INT PRIMARY KEY IDENTITY(1,1),
-    book_id INT,
-    quantity INT,
-    stage VARCHAR(255),
+    warehouse_id    INT PRIMARY KEY IDENTITY(1,1),
+    book_id         INT,
+    quantity        INT,
+    stage           VARCHAR(100),
     FOREIGN KEY (book_id) REFERENCES books.bookMarc(book_id)
 );
 GO
