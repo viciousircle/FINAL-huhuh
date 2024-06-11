54f325a648a2352a1ea82811ced97f9be0eab382
Vu Minh Quy <quy.vtm227257@sis.hust.edu.vn>
2024-06-11 14:15:45 +0700

    edit lms_type.py

--- lms_types.py
+++ lms_types.py
@@ -7,15 +7,16 @@ T = TypeVar('T')
 
 @dataclass
 class UsersAccountData:
-    admin_id: Optional[int]
+    admin_id: Optional[str]
     password: Optional[str]
     admin_name: Optional[str]
 
 @dataclass
 class UsersHistoryData:
     edit_id: Optional[int]
-    admin_id: Optional[int]
+    admin_id: Optional[str]
     book_id: Optional[int]
+    isbn: Optional[str]
     timestamp: Optional[datetime]
 
 @dataclass
@@ -42,6 +43,7 @@ class BooksBookMarcData:
 class BooksBookData:
     quantity: Optional[int]
     stage: Optional[str]
+    isbn : Optional[str]
     book_id: Optional[int] = field(default=None)
     warehouse_id: Optional[int] = field(default=None)
 
